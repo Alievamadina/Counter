fe7d08e1b44a5c12483cf7a6412cd4bba21e0f24
Madina <Alievamadin7@gmail.com>
2023-01-10 11:12:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { useState } from "react";
-import './App.css'
+import React from 'react'
+
 const App = () => {
-  const [count, setCount] = useState(0);
-  const inc = () => {
-    setCount(count + 1);
-  };
-  const dec = () => {
-    if (count > 0) setCount(count - 1);
-  };
   return (
-    <div className="ayat">
-      <h1>Субхан-Аллах <p/>
-سُبْحَانَ ٱللَّٰهِ</h1>
-<h1> {count}</h1>
-      <button onClick={inc}>Считать</button>
-      <div onClick={dec}>-</div>
+    <div>
+      
     </div>
-  );
-};
-export default App;
+  )
+}
+
+export default App
+
